76fd3df3cff30f88c235f347513623cf3f34bb77
anvir Anik@DESKTOP-NFJSQIG.(none) <tanvir.anik004@gmail.com>
2023-06-03 00:03:05 +0600

    comments

---
+++ comments.cpp
@@ -0,0 +1,14 @@
+//Single-line Comments
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    //Hi I am single line comments
+    cout <<"Hi I am Single line Comments\n";
+    /*
+    Hi i am Double line Comments
+    */
+    cout <<"Hi I am Double line Comments";
+    return 0;
+}
