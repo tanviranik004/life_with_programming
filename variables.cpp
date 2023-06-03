a291f8deba57eba916033cd6cac9c4fa60dbe5c8
anvir Anik@DESKTOP-NFJSQIG.(none) <tanvir.anik004@gmail.com>
2023-06-03 21:29:19 +0600

    Variables

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int myAge=20;
+    double myFloatNum=5.19;
+    char MyLatter='M';
+    string myText="Fariya";
+    bool myBoolean=true;
+
+    cout <<"I am " << myAge  << " years old\n";
+    cout <<"double is " << MyLatter << "\n";
+    cout <<"My Name  is " << myText << "\n";
+    cout <<"char is " << myBoolean;
+
+    return 0;
+
+}
