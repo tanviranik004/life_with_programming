709f94553f63f6ad88ba457a535c76088e99de04
anvir Anik@DESKTOP-NFJSQIG.(none) <tanvir.anik004@gmail.com>
2023-06-07 06:24:40 +0600

    user sum

---
+++ userin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b ;
+    int sum;
+
+    cout << "Enter a firstt number : ";
+    cin >> a;
+    cout << "Enter a second number : ";
+    cin >> b;
+
+    sum=a+b;
+
+    cout << "The sum is :" << sum ;
+
+    return 0;
+
+}
