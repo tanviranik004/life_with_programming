7ea7a27ebdd8eb943ee282c3009558608b738522
anvir Anik@DESKTOP-NFJSQIG.(none) <tanvir.anik004@gmail.com>
2023-06-10 23:00:06 +0600

    numbers

---
+++ numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+
+    int a=8;
+    float b=6.70;
+    double f=19.99;
+    cout <<f <<"\n" ;
+    //cout << "\n";
+    cout << b;
+    return 0;
+
+
+}
