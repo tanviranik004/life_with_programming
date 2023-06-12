3e1dd924053308ddbe46c6a3216e1fe0112490f3
anvir Anik@DESKTOP-NFJSQIG.(none) <tanvir.anik004@gmail.com>
2023-06-12 21:14:45 +0600

    string

---
+++ bool.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+        bool isCodingFun=true;
+        bool isFashingTasty=false;
+
+        cout<< isCodingFun <<"\n";
+        cout << isFashingTasty;
+
+
+
+}
---
+++ char.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+
+      char myGarde ='B';
+      char a = 65;
+      char b= 66;
+      char c=67;
+
+      cout << a;
+      cout << b;
+      cout << c;
+      
+      cout <<myGarde;
+
+      return 0;
+
+
+
+}
+
+
+
+
+
